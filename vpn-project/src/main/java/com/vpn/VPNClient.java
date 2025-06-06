c3c8bfc4516092b9c8e79eb67efdf2a3c9186393
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-06-06 20:41:40 +0530

    wrote code for client

--- vpn-project/src/main/java/com/vpn/VPNClient.java
+++ vpn-project/src/main/java/com/vpn/VPNClient.java
@@ -1,5 +1,23 @@
 package com.vpn;
 
+import java.io.*;
+import java.net.*;
+
 public class VPNClient {
-    
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 9000)) {
+            System.out.println("Connected to VPN Server");
+
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            out.println("Hello VPN Server");
+            String response = in.readLine();
+            System.out.println("Received from server: " + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
