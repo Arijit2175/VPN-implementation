965ad097c5961c7ed65bc2b65d191c54cbd74822
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-06-06 20:57:46 +0530

    generating RSA key pair

--- vpn-project/src/main/java/com/vpn/CryptoUtils.java
+++ vpn-project/src/main/java/com/vpn/CryptoUtils.java
@@ -6,5 +6,9 @@ import java.security.*;
 import java.util.Base64;
 
 public class CryptoUtils {
-    
+    public static KeyPair generateRSKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair(
+    }
 }
