c213d03d4be7eade6b7f9782a64c3bae93046d2f
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-06-09 13:46:35 +0530

    reading requests

--- vpn-project/src/main/java/com/vpn/VPNClientWithLogging.java
+++ vpn-project/src/main/java/com/vpn/VPNClientWithLogging.java
@@ -5,5 +5,12 @@ import java.io.*;
 import java.net.Socket;
 
 public class VPNClientWithLogging {
-    
+     public static void runClient(JTextArea logArea) {
+        try (Socket socket = new Socket("localhost", 9000);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+
+            log(logArea, "✅ Connected to VPN Server");
+
+            
 }
