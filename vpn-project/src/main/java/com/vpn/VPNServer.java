3983769360fd3fd6d70e0e656a46194bb21eb598
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-06-09 13:21:40 +0530

    generate rsa key pair

--- vpn-project/src/main/java/com/vpn/VPNServer.java
+++ vpn-project/src/main/java/com/vpn/VPNServer.java
@@ -2,6 +2,9 @@ package com.vpn;
 
 import java.io.*;
 import java.net.*;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 public class VPNServer {
     public static void main(String[] args) {
@@ -15,6 +18,10 @@ public class VPNServer {
                     new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out = new PrintWriter(
                     clientSocket.getOutputStream(), true);
+
+            KeyPair rsaKeyPair = CryptoUtils.generateRSAKeyPair();
+            PublicKey publicKey = rsaKeyPair.getPublic();
+            PrivateKey privateKey = rsaKeyPair.getPrivate();
 
             String clientMessage = in.readLine();
             System.out.println("Received from client: " + clientMessage);
