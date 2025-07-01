b8d1caeadba4ebc6a65deb5a027979b822d58d71
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-01 16:11:33 +0530

    updated code for packet response

--- vpn-project/src/main/java/com/vpn/VPNServer.java
+++ vpn-project/src/main/java/com/vpn/VPNServer.java
@@ -49,12 +49,18 @@ public class VPNServer {
 
             while (true) {
                 try {
-                    String packetBase64 = in.readUTF();
+                    String packetBase64 = in.readUTF();  
                     byte[] encryptedBytes = Base64.getDecoder().decode(packetBase64);
                     byte[] decryptedBytes = CryptoUtils.aesDecrypt(encryptedBytes, aesKey);
 
                     System.out.println("[Forwarded Packet] Length: " + decryptedBytes.length + " bytes");
                     System.out.println("Hex Preview: " + bytesToHexPreview(decryptedBytes));
+
+                    String dummyResponse = "ACK:" + decryptedBytes.length + " bytes";
+                    byte[] encRespBack = CryptoUtils.aesEncrypt(dummyResponse.getBytes(), aesKey);
+                    out.writeUTF(Base64.getEncoder().encodeToString(encRespBack));
+                    out.flush();
+
                 } catch (EOFException eof) {
                     System.out.println("Client disconnected.");
                     break;
