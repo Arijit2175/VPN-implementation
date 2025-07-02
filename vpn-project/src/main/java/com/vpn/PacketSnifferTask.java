588f18811b11447b9175564f38345dce8ec1b6ed
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-02 15:18:43 +0530

    modified the code

--- vpn-project/src/main/java/com/vpn/PacketSnifferTask.java
+++ vpn-project/src/main/java/com/vpn/PacketSnifferTask.java
@@ -11,7 +11,7 @@ public class PacketSnifferTask implements Runnable {
     private final JTextArea logArea;
     private final int interfaceIndex;
 
-     public PacketSnifferTask(JTextArea logArea, int interfaceIndex) {
+    public PacketSnifferTask(JTextArea logArea, int interfaceIndex) {
         this.logArea = logArea;
         this.interfaceIndex = interfaceIndex;
     }
@@ -20,7 +20,9 @@ public class PacketSnifferTask implements Runnable {
         SwingUtilities.invokeLater(() -> logArea.append("[Packet] " + msg + "\n"));
     }
 
+    @Override
     public void run() {
+        PcapHandle handle = null;
         try {
             List<PcapNetworkInterface> interfaces = Pcaps.findAllDevs();
             if (interfaceIndex < 0 || interfaceIndex >= interfaces.size()) {
@@ -31,20 +33,24 @@ public class PacketSnifferTask implements Runnable {
             PcapNetworkInterface nif = interfaces.get(interfaceIndex);
             log("Sniffing on: " + nif.getName());
 
-             int snapLen = 65536;
+            int snapLen = 65536;
             int timeout = 10;
-            PcapHandle handle = nif.openLive(snapLen, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, timeout);
+            handle = nif.openLive(snapLen, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, timeout);
 
             PacketListener listener = packet -> {
+                if (Thread.currentThread().isInterrupted()) return;
                 String summary = packet.toString().split("\n")[0];
                 log("Captured: " + summary);
             };
 
-            handle.loop(20, listener);  
-            handle.close();
+            handle.loop(-1, listener);
 
         } catch (Exception e) {
             log("Sniffer error: " + e.getMessage());
+        } finally {
+            if (handle != null && handle.isOpen()) {
+                handle.close();
+            }
         }
     }
 }
