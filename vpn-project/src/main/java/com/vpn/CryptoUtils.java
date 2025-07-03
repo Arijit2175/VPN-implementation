d3e3894b8dc161cf2e9610a0503d8af7038c059e
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-03 15:49:58 +0530

    end

--- vpn-project/src/main/java/com/vpn/CryptoUtils.java
+++ vpn-project/src/main/java/com/vpn/CryptoUtils.java
@@ -6,46 +6,54 @@ import java.security.*;
 import java.util.Base64;
 
 public class CryptoUtils {
+    // Generate a new RSA key pair for secure key exchange
     public static KeyPair generateRSAKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(2048);
         return keyGen.generateKeyPair();
     }
 
+    // Generate a new AES key for symmetric encryption
     public static SecretKey generateAESKey() throws NoSuchAlgorithmException {
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
         keyGen.init(128);
         return keyGen.generateKey();
     }
 
+    // Encrypt and decrypt data using RSA and AES
     public static byte[] rsaEncrypt(byte[] data, PublicKey publicKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
         return cipher.doFinal(data);
     }
 
+    // Decrypt data using RSA
     public static byte[] rsaDecrypt(byte[] data, PrivateKey privateKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
         return cipher.doFinal(data);
     }
 
+    // Encrypt and decrypt data using AES
     public static byte[] aesEncrypt(byte[] data, SecretKey aesKey) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, aesKey);
         return cipher.doFinal(data);
     }
 
+    // Decrypt data using AES
     public static byte[] aesDecrypt(byte[] data, SecretKey aesKey) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, aesKey);
         return cipher.doFinal(data);
     }
 
+    // Convert SecretKey to String and vice versa for easy storage/transmission
     public static String secretKeyToString(SecretKey key){
         return Base64.getEncoder().encodeToString(key.getEncoded());
     }   
 
+    // Convert String back to SecretKey
     public static SecretKey stringToSecretKey(String keyStr) {
         byte[] decoded = Base64.getDecoder().decode(keyStr);
         return new SecretKeySpec(decoded, 0, decoded.length, "AES");
